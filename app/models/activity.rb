70459a08849dae335c19f0b7c6e347d6e64edb57
hei.yoshii <y.heihachiro@gmail.com>
2020-02-04 02:10:50 +0000

    facing some error

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,7 @@
 class Activity < ApplicationRecord
   belongs_to :user
   belongs_to :lover
+  
+  validates :content, presence: true, length: {maximum: 32768}
+  validates :activity_date, presence: true
 end
--- app/models/lover.rb
+++ app/models/lover.rb
@@ -1,3 +1,6 @@
 class Lover < ApplicationRecord
   belongs_to :user
+  
+  validates :name, presence: true, length: {maximum: 255}
+  has_many :activities, dependent: :destroy
 end
